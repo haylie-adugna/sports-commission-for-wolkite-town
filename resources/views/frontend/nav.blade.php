36ba783973b692986c68fae1768d1097fab3253f
haylie <haylieadugna10@gmail.com>
2024-01-31 20:26:16 +0300

    updated

--- resources/views/frontend/nav.blade.php
+++ resources/views/frontend/nav.blade.php
@@ -31,7 +31,7 @@
 
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Sports
+                    Sport types
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Football</a>
