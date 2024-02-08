186f4745991fe71c2a4924e246f1828a52515616
haylie <haylieadugna10@gmail.com>
2024-02-08 13:27:20 +0300

    last update

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Str;
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => 'fasil',
+            'last_name' => 'Desalegn',
+            'email' => 'haylieadugna13@gmail.com',
+            'phone' => '123456789',
+            'password' => Hash::make('12345678'),
+            'verified' => true,
+            'email_verified_at' => now(),
+            'user_type' => 'commissioner',
+            'age' => '23',
+            'gender' => 'male',
+            'date_of_birth' => '1999-01-01',
+            'address' => '123 Main St',
+            'country' => 'Ethiiopia',
+            'nationality' => 'Ethiopian',
+            'city' => 'Wolkitie',
+            'language' => 'Amharic',
+            'behavioral_record' => 'Good',
+            'photo' => 'user_photo.jpg',
+            'assigned_game' => 'Football',
+            'experience' => '3 years',
+            'mother_full_name' => 'ayehu anteneh',
+            'status' => 'active',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Add more seed data if needed
+    }
+}
