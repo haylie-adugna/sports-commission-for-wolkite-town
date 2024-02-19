e1e11161ff2a1fff40da18160172daa2bcca5601
haylie <haylieadugna10@gmail.com>
2024-02-19 22:30:47 +0300

    last update

--- config/app.php
+++ config/app.php
@@ -55,9 +55,9 @@ return [
     |
     */
 
-     'url' => env('APP_URL', 'http://10.194.111.11'),
+     'url' => env('APP_URL', 'http://10.194.111.27'),
 
-     'asset_url' => env('ASSET_URL', 'http://10.194.111.11/module/Wolkite-town-sport-commission/public'),
+     'asset_url' => env('ASSET_URL', 'http://10.194.111.27/module/Wolkite-town-sport-commission/public'),
 
     //  'url' => env('APP_URL', 'http://localhost'),
 
