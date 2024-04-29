bc0e7eda9e59919d661a29b0f33ddc2e5707956b
haylie <haylieadugna10@gmail.com>
2024-04-29 21:07:25 +0000

    last update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
 
-    @extends('welcome') <!-- Adjust the layout as needed -->
-
-    @section('content')
 <head>
-	
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
 	<title>error 404</title>
 
 	<!-- Google font -->
